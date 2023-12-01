a798552cf9d3aaf8afcb6a776b8946f8b7ca1c07
Fernando Crespo <fernando82@gmail.com>
2023-12-01 18:05:09 -0300

    Add WSL alias

--- aliases.zsh
+++ aliases.zsh
@@ -20,8 +20,11 @@ fi
 
 alias yt-dlp-mp4="yt-dlp -S res,ext:mp4:m4a --recode mp4"
 alias restartexplorer="taskkill.exe /f /im explorer.exe; explorer.exe &"
+alias debug="exec env DEBUG=1 zsh"
+
+#Windows aliases
 alias explorer="explorer.exe"
 alias winget="winget.exe"
+alias wsl="wsl.exe"
 alias taskkill="taskkill.exe"
 alias nslookup="nslookup.exe"
-alias debug="exec env DEBUG=1 zsh"
