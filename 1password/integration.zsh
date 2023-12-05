59157f61cecd47a66c1ebc0b56f4dfb40a97d6ae
Fernando Crespo <fernando82@gmail.com>
2023-12-04 23:15:39 -0300

    Changing integration to native 1password

--- 1password/integration.zsh
+++ 1password/integration.zsh
@@ -1,8 +1,17 @@
-export OP_INTEGRATION_SCRIPT=${ZDOTDIR:-$HOME}/1password/agent-bridge.sh
+# export OP_INTEGRATION_SCRIPT=${ZDOTDIR:-$HOME}/1password/agent-bridge.sh
 
-reset-1password-integration() {
-  export OP_SOCAT_PID=$(pgrep -f openssh-ssh-agent)
-  kill -9 $OP_SOCAT_PID >/dev/null 2>&1
-  source $OP_INTEGRATION_SCRIPT
-}
-alias reset1p="reset-1password-integration"
+# reset-1password-integration() {
+#   export OP_SOCAT_PID=$(pgrep -f openssh-ssh-agent)
+#   kill -9 $OP_SOCAT_PID >/dev/null 2>&1
+#   source $OP_INTEGRATION_SCRIPT
+# }
+# alias reset1p="reset-1password-integration"
+
+CORE_SSH_COMMAND=`git config --global core.sshCommand`
+
+if [ -z "$CORE_SSH_COMMAND" ]; then
+    git config --global core.sshCommand ssh.exe
+fi
+
+alias ssh='ssh.exe'
+alias ssh-add='ssh-add.exe'
